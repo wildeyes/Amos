92a3fd3f685b4e11dc50eef3dbdc293390d453ca
xwildeyes <xwildeyes@gmail.com>
2014-07-22 14:46:17 +0300

    update to work

--- script.js
+++ script.js
@@ -4,17 +4,36 @@
 // @version    0.1.0
 // @description Add features to Trello - UserScript (use w/ TamperMonkey / GreaseMonkey)
 // @match      http*://trello.com/*
+// @grant none
 // @copyright  2014+, XWILDEYES
 // ==/UserScript==
 
 window.lstchklst = [];
 
-window.onpopstate = history.onpushstate = function(e) {
+(function(history) {
+  var pushState = history.pushState;
+  history.pushState = function(state) {
+    if (typeof history.onpushstate == "function") {
+      history.onpushstate({
+        state: state
+      });
+    }
+
+    handler();
+
+    return pushState.apply(history, arguments);
+  }
+})(window.history);
+
+handler = function(e) {
   var card_id_expr = window.location.href.match(/c\/\w+\/(\d+)/),
     board_id_expr = window.location.href.match(/b\/\w+\/(\w+)/),
-    board_id = !board_id_expr.length ? 0 : board_id_expr[board_id_expr.length - 1],
-    card_id = !card_id_expr.length ? 0 : card_id_expr[card_id_expr.length - 1],
-    lst = window.lstchklst
+    board_id = !board_id_expr ? 0 : board_id_expr[board_id_expr.length - 1],
+    card_id = !card_id_expr ? 0 : card_id_expr[card_id_expr.length - 1],
+    lst = window.lstchklst,
+    id = 0,
+    $card = null,
+    $last = null
 
   if (card_id) {
 
@@ -22,16 +41,27 @@ window.onpopstate = history.onpushstate = function(e) {
       .find(".checklist-item-details-text")
       .eq(0)
       .text()
+    $card = $(".list-card-details:contains('" + card_id + "')").eq(0)
+    $last = $card.find(".lstchklst")
+    if ($last.length) {
+      $last.text(lst[id])
+    } else {
 
-    $("<span>" + lst[id] + "</span>")
-      .addClass("badge-text")
-      .css("margin-left", "5px")
-      .appendTo($(".list-card-details:contains('" + card_id + "')").find(".badge"))
+      $last = $("<span>" + lst[id] + "</span>")
+        .addClass("lstchklst badge-text")
+        .css("margin-left", "5px")
+        .appendTo($card.find(".badge").last())
+      //               .addClass("lstchklst badges badge-text")
+      //               .css("margin-left","5px")
+      //               .appendTo($card.find("list-card-details clearfix").eq(0))
+
+    }
+    console.log($last)
   } else if (board_id) {
     //$(".list-card-details:contains(" + board_id + ")")
   }
 };
-
+history.p
 //= window.sup;
 
 //console.log("Trello Last Checklist Item Loaded!")
